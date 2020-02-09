aaa8749ebf45cedfb8069947b7e2cc84019ca7a5
Tanisha Howell <57296464+tanishahowell@users.noreply.github.com>
2020-02-09 15:44:17 -0500

    updated html & js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # unit-4-game
+Crystal Collector!
+
+Here's How to Play:
+
+You will be given a random number at the start of the game.
+
+Click the crystals to add points to your total score. Points are hidden until clicked!
+
+You win the game by matching your total score to random number. You lose the game if your total score goes abve the random number.
+
+Each time when the game starts, the game will change the values of eaach crystal.
+
+Good Luck!
+
+![Image of Crystal Collectors Game](assets\images\crystalCollector.png)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,139 @@
-var crystal1 = 
+//GLOBAL VARIABLES FUNCTIONS
+//=========================================
+//Crystal Variables
+var crystal = {
+    green:
+    {
+        name: "Green",
+        value: 0
+    },
+    multicolored:
+    {
+        name: "Multicolored",
+        value: 0
+    },
+    purple:
+    {
+        name: "Purple",
+        value: 0
+    },
+    blue:
+    {
+        name: "Blue",
+        value: 0
+    }
+};
 
-var total-score =
+// Scores Current and Target
+var currentScore = 0;
+var targetScore = 0;
 
-var randomNumber =
+//Wins and Losses
+var winCount = 0;
+var lossCount = 0;
+
+//FUNCTIONS
+//=========================================
+//Helper function for getting random numbers
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Starts and restarts the game
+var startGame = function() {
+    //Reset current score
+    currentScore = 0;
+    //Set new target scor (between 19 and 120)
+    targetScore = getRandom(19, 120);
+
+    //Set different values for each of the crystals (betreen 1 and 12)
+    crystal.green.value = getRandom(1, 12);
+    crystal.multicolored.value = getRandom(1, 12);
+    crystal.purple.value = getRandom(1, 12);
+    crystal.blue.value = getRandom(1, 12);
+
+    //Change the HTML to reflect all of these changes
+    $("#yourScore").html(currentScore);
+    $("#targetScore").html(targetScore);
+
+
+    //Testing Console
+    console.log("-----------------")
+    console.log("Target Scoore: "+ targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Red: " + crystal.red.value + " | Green: " + crystal.green.value + " | Yellow: " + crystal.yellow.value);
+    console.log("-----------------")
+
+}
+
+//Respond to clicks on the crystals
+var addValues = function(crystal){
+
+    //Change currentScore
+    currentScore = currentScore + crystal.value;
+
+    //Change the HTML to reflect changes in currentScore
+    $("#yourScore").html(currentScore);
+
+    //Call the checkWin function
+    checkWin();
+
+    //Testing
+    console.log("Your score: " + currentScore);
+
+}
+
+//Check if user won or lost and reset the game
+var checkWin = function(){
+
+}
+
+//Check if currentScore is larger than targetScore
+if(currentScore > targetScore) {
+    alert("Sorry. You lost!");
+    console.log("You lost");
+    
+//Add to loss counter
+lossCount++;
+
+//Change Loss Count HTML
+$("#lossCount").html(lossCount);
+
+
+} else if (currentScore == targetScore) {
+    alert("Congratulations! You won!");
+    console.log("You Won!");
+
+//Add to the Win Counter
+winCount++;
+//Restart the game
+startGame();
+
+//Change Win Count HTML
+$("#winCount").html(winCount);
+
+//Restart the game
+startGame();
+}
+
+//MAIN PROCESS
+//=========================================
+
+//Starts the game the first time
+startGame();
+
+$("#green").on("click", function() {
+    addValues(crystal.green);
+});
+
+$("#multicolored").on("click", function() {
+    addValues(crystal.multicolored);
+});
+
+$("#purple").on("click", function() {
+    addValues(crystal.purple);
+});
+
+$("#blue").on("click", function() {
+    addValues(crystal.blue);
+});
+
