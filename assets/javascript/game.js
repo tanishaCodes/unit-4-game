59574d61e4a3d54b6601b2d1b0393f413fb2de10
Tanisha Howell <57296464+tanishahowell@users.noreply.github.com>
2020-02-12 20:11:13 -0500

    fixed bug in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,12 +25,12 @@ var crystal = {
 };
 
 // Scores Current and Target
-var currentScore = 0;
-var targetScore = 0;
+var currentScore    = 0;
+var targetScore     = 0;
 
 //Wins and Losses
-var winCount = 0;
-var lossCount = 0;
+var winCount        = 0;
+var lossCount       = 0;
 
 //FUNCTIONS
 //=========================================
@@ -47,10 +47,10 @@ var startGame = function() {
     targetScore = getRandom(19, 120);
 
     //Set different values for each of the crystals (betreen 1 and 12)
-    crystal.green.value = getRandom(1, 12);
-    crystal.multicolored.value = getRandom(1, 12);
-    crystal.purple.value = getRandom(1, 12);
-    crystal.blue.value = getRandom(1, 12);
+    crystal.green.value         = getRandom(1, 12);
+    crystal.multicolored.value  = getRandom(1, 12);
+    crystal.purple.value        = getRandom(1, 12);
+    crystal.blue.value          = getRandom(1, 12);
 
     //Change the HTML to reflect all of these changes
     $("#yourScore").html(currentScore);
@@ -59,14 +59,13 @@ var startGame = function() {
 
     //Testing Console
     console.log("-----------------")
-    console.log("Target Scoore: "+ targetScore);
-    console.log("Blue: " + crystal.blue.value + " | Red: " + crystal.red.value + " | Green: " + crystal.green.value + " | Yellow: " + crystal.yellow.value);
+    console.log("Target Score: " + targetScore);
+    console.log("Green: " + crystal.green.value + " | Multicolored: " + crystal.multicolored.value + " | Purple: " + crystal.purple.value + " | Blue: " + crystal.blue.value);
     console.log("-----------------")
-
 }
 
 //Respond to clicks on the crystals
-var addValues = function(crystal){
+var addValues = function(crystal) {
 
     //Change currentScore
     currentScore = currentScore + crystal.value;
@@ -79,42 +78,40 @@ var addValues = function(crystal){
 
     //Testing
     console.log("Your score: " + currentScore);
-
 }
 
 //Check if user won or lost and reset the game
-var checkWin = function(){
+var checkWin = function() {
 
+    //Check if currentScore is larger than targetScore
+    if(currentScore > targetScore) {
+        alert("Sorry. You lost!");
+        console.log("You lost");
+
+    //Add to loss counter
+        lossCount++;
+
+    //Change Loss Count HTML
+        $("#lossCount").html(lossCount);
+
+    //Restart the game
+        startGame();
+    }
+
+    else if(currentScore == targetScore) {
+        alert("Congratulations! You won!");
+        console.log("You Won!");
+
+    //Add to the Win Counter
+        winCount++;
+
+    //Change Win Count HTML
+        $("#winCount").html(winCount);
+
+    //Restart the game
+        startGame();
+    }
 }
-
-//Check if currentScore is larger than targetScore
-if(currentScore > targetScore) {
-    alert("Sorry. You lost!");
-    console.log("You lost");
-    
-//Add to loss counter
-lossCount++;
-
-//Change Loss Count HTML
-$("#lossCount").html(lossCount);
-
-//Restart the game
-startGame();
-
-} else if (currentScore == targetScore) {
-    alert("Congratulations! You won!");
-    console.log("You Won!");
-
-//Add to the Win Counter
-winCount++;
-
-//Change Win Count HTML
-$("#winCount").html(winCount);
-
-//Restart the game
-startGame();
-}
-
 //MAIN PROCESS
 //=========================================
 
@@ -135,5 +132,4 @@ $("#purple").on("click", function() {
 
 $("#blue").on("click", function() {
     addValues(crystal.blue);
-});
-
+})
